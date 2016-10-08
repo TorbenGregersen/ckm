2d17452398c3a626b5eb17b6c97c4666ff393043
TorbenGregersen <flt6000@gmail.com>
2016-10-08 10:19:22 +0200

    added migrations and model

--- database/migrations/2016_10_08_075839_create_categories_table.php
+++ database/migrations/2016_10_08_075839_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('content_type');
-            $table->integer('parent_id')->unsigned()-default(0);
+            $table->integer('parent_id')->unsigned()->default(0);
             $table->timestamps();
         });
     }
